4723b95a58c9b9ce3e1a0a6bc486cf6a43556d66
AngelTintaya <a.tintaya.m@gmail.com>
2024-12-20 18:51:34 -0500

    Storing datetime utcnow properly

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ db = SQLAlchemy(app)
 # Log Table Datamodel
 class Log(db.Model):
     id=db.Column(db.Integer, primary_key=True)
-    fecha_y_hora = db.Column(db.DateTime, default=datetime.utcnow())
+    fecha_y_hora = db.Column(db.DateTime, default=datetime.utcnow)
     texto = db.Column(db.TEXT)
 
 # Creating table if not exists
