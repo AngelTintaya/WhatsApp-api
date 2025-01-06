2e5cb9b33445d4e8388dd71698b0e4c22f0e4921
AngelTintaya <a.tintaya.m@gmail.com>
2025-01-06 15:21:02 -0500

    Making ai conversation smooth

--- agent_ai.py
+++ agent_ai.py
@@ -27,16 +27,16 @@ def ask_eva(query: str, phone_number:int, rules: List[str] = None) -> str:
         # model=OpenAIChat(id="gpt-4o"),
         model=Groq(id="llama-3.3-70b-Versatile"),
         storage=SqlAgentStorage(table_name="agent_sessions", db_file="tmp/agent_storage.db"),
-        description="Asistente del concesionario de autos: Connectia, para programar citas y responder preguntas.",
+        description="Eva, asistente del concesionario de autos Connectia, especializada en programar citas y responder preguntas."
         task="Gestionar citas y consultas sobre mantenimiento de autos.",
+
         instructions=[
-            f"Hoy es {today_date}. Utiliza esta fecha como referencia para fechas relativas o validación."
-            "Pregunta el nombre del usuario en la primera interacción.",
+            f"Hoy es {today_date}. Utiliza esta fecha como referencia para fechas relativas o validación.",
+            "Inicia presentandote y pregunta amablemente el nombre del usuario.",
             "Ayuda a programar citas de mantenimiento y responder preguntas.",
             "Pregunta por fecha y hora futura. Si falta el año, usa el actual o pide confirmación.",
             "Valida que la fecha proporcionada sea válida (existe en el calendario). Si no es válida, pide una fecha diferente.",
-            "Confirma la fecha: 'Programada para [Fecha (dd/yy/MMMM)], ¿es correcto?'. Si es una fecha pasada o inválida, pide otra fecha.",
-            "Para fechas relativas, infiere la fecha y confirma.",
+            "Confirma la fecha de la cita de forma clara: '¿Te gustaría agendar la cita para el [Fecha (dd/yy/MMMM)]?'. Si la fecha no es válida o es una fecha pasada, pide otra fecha.",
             "Mantén las respuestas cortas, enfocándote en la fecha, hora y confirmación.",
             "Responde brevemente a preguntas sobre el taller, priorizando las citas."
         ],
